2f518f3e719caea41c90b7739b3804f71645f2d0
Merten Dieckmann <merten.dieckmann@web.de>
2023-03-17 10:27:59 +0100

    feat: Added zoom animation to badge dialog

--- src/components/info/BadgeDialog.js
+++ src/components/info/BadgeDialog.js
@@ -1,52 +1,57 @@
 import Dialog from '@mui/material/Dialog';
 import {useVariablesStore} from "../../stores/variablesStore";
-import {DialogContent, DialogTitle} from "@mui/material";
+import {DialogContent, DialogTitle, Zoom} from "@mui/material";
 import React from "react";
 import ConfirmButton from "../controls/ConfirmButton";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Zoom timeout={500} ref={ref} {...props} />;
+});
 
 export function BadgeDialog() {
 
     const variableState = useVariablesStore()
 
     return (
-        <Dialog
-            open={variableState.isBadgeDialogOpen}
-            onClose={variableState.closeBadgeDialog}
-            style={{
-                backgroundColor: "#99999930"
-            }}
-            PaperProps={{ sx: {
-                border: 2,
-                borderColor: "#22935B",
-                borderRadius: 4,
-                backgroundColor: "transparent"
-            }}}
-        >
-            <div style={{
-                backgroundColor: "#282c34"
-            }}>
-                <DialogTitle style={{
-                    color: "white",
-                    textAlign: "center"
-                }}>You Received A Badge</DialogTitle>
-                <DialogContent>
-                    <div style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}>
+            <Dialog
+                open={variableState.isBadgeDialogOpen}
+                onClose={variableState.closeBadgeDialog}
+                style={{
+                    backgroundColor: "#99999930"
+                }}
+                PaperProps={{ sx: {
+                    border: 2,
+                    borderColor: "#22935B",
+                    borderRadius: 4,
+                    backgroundColor: "transparent"
+                }}}
+                TransitionComponent={Transition}
+            >
+                <div style={{
+                    backgroundColor: "#282c34"
+                }}>
+                    <DialogTitle style={{
+                        color: "white",
+                        textAlign: "center"
+                    }}>You Received A Badge</DialogTitle>
+                    <DialogContent>
                         <div style={{
-                            color: "#22935B",
-                            fontWeight: "bold",
-                            fontSize: 35,
-                            marginBottom: 15
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
                         }}>
-                            { variableState.unlockedBadgeName }
+                            <div style={{
+                                color: "#22935B",
+                                fontWeight: "bold",
+                                fontSize: 35,
+                                marginBottom: 15
+                            }}>
+                                { variableState.unlockedBadgeName }
+                            </div>
+                            <ConfirmButton onConfirm={() => variableState.closeBadgeDialog()} />
                         </div>
-                        <ConfirmButton onConfirm={() => variableState.closeBadgeDialog()} />
-                    </div>
-                </DialogContent>
-            </div>
-        </Dialog>
+                    </DialogContent>
+                </div>
+            </Dialog>
     );
 }
